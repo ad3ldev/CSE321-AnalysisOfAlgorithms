6bcbbf9873831c72a8c7f1bef9b951b242d796c5
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2022-11-12 01:48:59 +0200

    Update Fibonacci.java

--- src/sheet4/Fibonacci.java
+++ src/sheet4/Fibonacci.java
@@ -1,6 +1,9 @@
 package sheet4;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
+
 public class Fibonacci {
     public static BigInteger[][] multiply2x2Matrices(BigInteger[][] a, BigInteger[][] b){
         BigInteger c00 = a[0][0].multiply(b[0][0]).add(a[0][1].multiply(b[1][0]));
@@ -9,25 +12,27 @@ public class Fibonacci {
         BigInteger c11 = a[1][0].multiply(b[0][1]).add(a[1][1].multiply(b[1][1]));
         return new BigInteger[][] {{c00, c01},{c10, c11}};
     }
+
     public static BigInteger[][] matPower(BigInteger[][] mat, BigInteger exponent) {
-        for (BigInteger i = BigInteger.valueOf(1); !i.equals(exponent); i = i.add(BigInteger.valueOf(1))){
-            BigInteger smallerTemp = mat[1][0];
-            BigInteger biggerTemp = mat[1][1];
-            mat[1][0] = mat[1][0].add(mat[0][0]);
-            mat[1][1] = mat[1][1].add(mat[0][1]);
-            mat[0][0] = smallerTemp;
-            mat[0][1] = biggerTemp;
+        if(exponent.equals(BigInteger.ONE)){
+            return mat;
         }
-        return mat;
+        BigInteger[][] temp = matPower(mat, exponent.divide(BigInteger.TWO));
+        if ( exponent.mod(BigInteger.TWO).equals(BigInteger.ZERO)){
+            return multiply2x2Matrices(temp, temp);
+        }else{
+            return multiply2x2Matrices(multiply2x2Matrices(temp,temp), mat);
+        }
     }
     public static BigInteger getNthFibonacciNumber(BigInteger n) {
         BigInteger[][] b = new BigInteger[][] {{BigInteger.ZERO, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ONE}};
         return matPower(b, n)[0][1]; // Replace the question marks by correct values.
     }
     public static void main(String[] args) {
-        BigInteger last = getNthFibonacciNumber(BigInteger.valueOf(10000));
-        BigInteger beforeLast = getNthFibonacciNumber(BigInteger.valueOf(9999));
-        BigInteger ratio = last.divide(beforeLast);
-        System.out.println(last);
+        System.out.println(getNthFibonacciNumber(BigInteger.valueOf(10000)));
+        BigDecimal last = new BigDecimal(getNthFibonacciNumber(BigInteger.valueOf(10000)));
+        BigDecimal beforeLast = new BigDecimal(getNthFibonacciNumber(BigInteger.valueOf(9999)));
+        BigDecimal ratio = last.divide(beforeLast, MathContext.DECIMAL64);
+        System.out.println("Ratio: "+ ratio);
     }
 }
